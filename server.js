4215b3f937558db5e36d020e9269232a85faafb3
Felecia Davis <fmdavis1@gmail.com>
2022-06-06 05:45:49 -0400

    Greeting express application created.

---
+++ server.js
@@ -0,0 +1,34 @@
+//load express
+const express = require('express')
+//create an instance of express
+const app = express()
+const PORT = 3000
+
+//Middleware functions
+//They update the request as soon as they come in.
+
+// app.use((req, res, next) => {
+//     console.log('Running the middleware function!')
+//     next() //Go to the next middleware or the response.
+// })
+
+//JSON Middleware
+app.use(express.json())
+
+//if we don't need to read data from the url
+app.use(express.urlencoded({extended: false}))
+
+//Set up view engine
+app.set('view engine', 'ejs')
+app.set('views', './Views')
+
+//Root route
+app.get('/greetings/:name', (req, res) => {
+    res.send(`Greetings, ${req.params.name}!`)
+})
+
+
+
+app.listen(PORT, () => {
+    console.log('Server is running...')
+})
